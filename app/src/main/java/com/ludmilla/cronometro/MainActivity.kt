0139255906e73acc01ba5614c22eea5c9230d502
ludmillasilva <ludmilla.silva44@gmail.com>
2022-02-14 12:15:40 -0300

    Cronometro utilizando ViewBinding e explicando atraves de comentários como utilizar com Synthetics e com findViewById

--- app/src/main/java/com/ludmilla/cronometro/MainActivity.kt
+++ app/src/main/java/com/ludmilla/cronometro/MainActivity.kt
@@ -3,26 +3,74 @@ package com.ludmilla.cronometro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.Chronometer
+import com.ludmilla.cronometro.databinding.ActivityMainBinding
+// ================== SYNTHETICS =======================
+//Atençao: O SYNTHETICS É CONSIDERADO COMO OBSOLETO
+//Para utilizar o Synthetics, é necessario colocar esse import abaixo
+//e no build.grade id 'kotlin-android-extensions' e sincronizar. Apos isso,
+//colocar diretamente o id das variaveis do xml no evento de onClick
+//exemplo: pause.setOnClickListener
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     var running = false
     var pause: Long = 0
 
+// ========FINDVIEWBYID===========
+//Para Utilizar o findViewById, é necessario criar as variaveis abaixo
+//e chama-las dentro do OnCreate
+/*
+    lateinit var cronometro: Chronometer
+    lateinit var iniciar: Button
+    lateinit var pausar: Button
+    lateinit var zerar: Button
+*/
+
+//======== UTILIZANDO VIEWBINDING =============
+//Tambem sera necessario colocar no arquivo build.gradle
+//  buildFeatures{
+//        viewBinding true
+//    }
+//E sincronizar
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnStart.setOnClickListener {
+//Para utilizar o Synthetics ou  findViewById, é necessario descomentar
+//a linha abaixo
+
+        //setContentView(R.layout.activity_main)
+
+// Utilizando findViewById dentro do OnCreate, conforme citado
+// E as variaveis foram criadas para serem chamadas no OnClick
+//exemplo: iniciar.setOnClickListener
+
+/*
+        cronometro = findViewById(R.id.chronometer)
+        iniciar = findViewById(R.id.btnStart)
+        pausar = findViewById(R.id.btnPause)
+        zerar = findViewById(R.id.btnZero)
+*/
+
+       binding.btnStart.setOnClickListener {
             IniciarCronometro()
         }
 
-        btnPause.setOnClickListener {
+        binding.btnPause.setOnClickListener {
             PausarCronometro()
         }
 
-        btnZero.setOnClickListener {
+        binding.btnZero.setOnClickListener {
             ZerarCronometro()
         }
 
@@ -30,20 +78,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun IniciarCronometro() {
         if (!running) {
-            chronometer.base = SystemClock.elapsedRealtime() - pause
-            chronometer.start()
+            binding.chronometer.base = SystemClock.elapsedRealtime() - pause
+            binding.chronometer.start()
             running = true
         }
     }
     private fun PausarCronometro(){
         if(running){
-        chronometer.stop()
-        pause = SystemClock.elapsedRealtime() - chronometer.base
+        binding.chronometer.stop()
+        pause = SystemClock.elapsedRealtime() - binding.chronometer.base
         running = false
         }
     }
     private fun ZerarCronometro(){
-        chronometer.base = SystemClock.elapsedRealtime()
+        binding.chronometer.base = SystemClock.elapsedRealtime()
         pause = 0
     }
 
